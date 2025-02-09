db225a6b75457a5e71e94f2faaf80146c23b3907
eddy <106661971+eddiemws@users.noreply.github.com>
2025-02-09 23:52:39 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,7 @@
 import tkinter as tk
 
 # Function to add a number or operator to the display
-def button_click(item):
-    current = display.get()
-    if item in '0123456789':
-        display.insert(tk.END, str(item))
-    elif item in '+-*/':
-        if current and current[-1] not in '+-*/':
-            display.insert(tk.END, str(item))
+
 
 # Function to clear the display
 def button_clear():
